b632394202ccb45ba3f6bfa2845330c03ef4bb82
Emirhan-Kara <emirhankara@stu.khas.edu.tr>
2024-10-29 13:29:18 +0300

    Objective 2 is done, some methdods are merged with objective2.1 branch.

--- Project1_Group17.java
+++ Project1_Group17.java
@@ -218,14 +218,16 @@ public class Project1_Group17
         switch (input)
         {
             case "1":
-                transposeMatrix();
+                Obj2Case1();
                 break;
             case "2":
-                inverseMatrix();
+                Obj2Case2();
                 break;
             case "3":
+                Obj2Case3();
                 break;
             case "4":
+                Obj2Case4();
                 break;
             case "5":
                 returnHomePage();
@@ -236,16 +238,17 @@ public class Project1_Group17
         System.out.println("IMPORTANT NOTES");
         System.out.println("----------------");
         System.out.println("*  Option 2 (Inverse) requires a square matrix.\n");
-        System.out.println("*  Option 3 (Matrix Multiplication): The second matrix's row size will match the first matrix's column size automatically for compatibility.\n\n\n\n");
+        System.out.println("*  Option 3 (Matrix multiplication): The second matrix's row size will match the first matrix's column size automatically for compatibility.\n");
+        System.out.println("*  Option 4 (Element-wise matrix multiplication): Both matrices must have same dimensions for compatibility.\n\n\n\n");
         System.out.println("        1)   Transpose of a matrix\n");
         System.out.println("        2)   Inverse of a matrix\n");
-        System.out.println("        3)   Matrix Multiplication\n");
-        System.out.println("        4)   Element-wise multiplication\n");
+        System.out.println("        3)   Matrix multiplication\n");
+        System.out.println("        4)   Element-wise matrix multiplication\n");
         System.out.println("        5)   EXIT TO HOMEPAGE\n\n\n");
         System.out.print("        Please select an option from the list above(1-5)): ");
     }
     
-    public static void transposeMatrix()
+    public static void Obj2Case1()
     {
         int row = takeInputOfRowAndColumn("number of ROWS");
         int col = takeInputOfRowAndColumn("number of COLUMNS");
@@ -254,12 +257,13 @@ public class Project1_Group17
 
         fillMatrix(matrix, row, col);
         cls();
-        printMatrix(matrix, row, col);
-        printTranspose(matrix, row, col);
+        double transpose[][] = returnTranspose(matrix);
+        printMatrix(matrix, row, col, "Your Matrix:");
+        printMatrix(transpose, transpose.length, transpose[0].length, "Transpose of Your Matrix:");
         if (loopAsk())
         {
             loadingPage();
-            transposeMatrix();
+            Obj2Case1();
         }
         else
         {
@@ -307,9 +311,9 @@ public class Project1_Group17
             }
         }
     }
-    public static void printMatrix(double[][] mat, int row, int col)
+    public static void printMatrix(double[][] mat, int row, int col, String message)
     {
-        System.out.printf("\n\nYour %d x %d Matrix:\n", row, col);
+        System.out.printf("\n\n%s\n", message);
         System.out.println("=".repeat((col * 8)+3));
     
         for (int i = 0; i < row; i++)
@@ -321,24 +325,9 @@ public class Project1_Group17
             System.out.println();
         }
     
-        System.out.println("=".repeat((col * 8)+3) + "\n"); // Bottom divider line
+        System.out.println("=".repeat((col * 8)+3) + "\n\n"); // Bottom divider line
     }  
-    public static void printTranspose(double[][] mat, int row, int col)
-    {
-        System.out.printf("\n\nTranspose of Your %d x %d Matrix:\n", row, col);
-        System.out.println("=".repeat((row * 8)+3));
-
-        for (int i = 0; i < col; i++)
-        {
-            for (int j = 0; j < row; j++)
-            {
-                System.out.printf("%7.2f ", mat[j][i]); // 7 spaces wide and 2 decimal places
-            }
-            System.out.println();
-        }
     
-        System.out.println("=".repeat((row * 8)+3) + "\n");
-    }
     public static double[][] returnTranspose(double mat[][])
     {
         int row = mat.length;
@@ -356,7 +345,7 @@ public class Project1_Group17
     }
 
 
-    public static void inverseMatrix()
+    public static void Obj2Case2()
     {
         int row = takeInputOfRowAndColumn("both number of ROWS and COLUMNS");
         
@@ -364,13 +353,26 @@ public class Project1_Group17
 
         fillMatrix(matrix, row, row);
         cls();
-        printMatrix(matrix, row, row);
-        printInverse(matrix);
+        printMatrix(matrix, row, row, "Your Matrix:");
 
+        double determinant = determinant(matrix);
+        if (determinant == 0.0)
+            System.err.println("\n\nSingular matrices (determinant = 0) does not have an inverse!!\n");
+        
+        else if (matrix.length == 1)
+            printMatrix(matrix, row, row, "Inverse of Your Matrix:");
+        
+        else
+        {
+            double inverse[][] = inverseMatrix(matrix, determinant);
+            printMatrix(inverse, inverse.length, inverse[0].length, "Inverse of Your Matrix:");
+        }
+            
+        
         if (loopAsk())
         {
             loadingPage();
-            inverseMatrix();
+            Obj2Case2();
         }
         else
         {
@@ -378,48 +380,19 @@ public class Project1_Group17
             Objective2();
         }
     }
-    public static void printInverse(double[][] mat)
+    public static double[][] inverseMatrix(double[][] mat, double determinant)
     {
-        double determinant = determinant(mat);
+        // create the adjoint matrix by taking the transpose of the cofactor matrix
+        double[][] cofactorMatrix = cofactorMatrix(mat);
+        double[][] adjointMatrix = returnTranspose(cofactorMatrix);
         
-        if (determinant == 0.0)
-        {
-            System.err.println("\n\nSingular matrices (determinant = 0) does not have an inverse!!\n");
-        }
-        else if (mat.length == 1)
-        {
-            System.out.printf("\n\nInverse of Your %d x %d Matrix:\n", mat.length, mat[0].length);
-            System.out.println("=".repeat((mat.length * 8)+3));
-            System.out.printf("%7.2f \n", mat[0][0]);
-            System.out.println("=".repeat((mat.length * 8)+3) + "\n");
-        }
-        else
-        {
-            // create the adjoint matrix by taking the transpose of the cofactor matrix
-            double[][] cofactorMatrix = cofactorMatrix(mat);
-            double[][] adjointMatrix = returnTranspose(cofactorMatrix);
-            
-            // use the formula to derive the inverse of the matrix (Inverse Matrix = Adjoint Matrix . 1/determinant)
-            matrixMultWithConstant(adjointMatrix, 1.0 / determinant);
-            
-            // print the inverse matrix
-            System.out.printf("\n\nInverse of Your %d x %d Matrix:\n", mat.length, mat[0].length);
-            System.out.println("=".repeat((adjointMatrix.length * 8)+3));
-
-            for (int i = 0; i < adjointMatrix.length; i++)
-            {
-                for (int j = 0; j < adjointMatrix[0].length; j++)
-                {
-                    System.out.printf("%7.2f ", adjointMatrix[i][j]); // 7 spaces wide and 2 decimal places
-                }
-                System.out.println();
-            }
+        // use the formula to derive the inverse of the matrix (Inverse Matrix = Adjoint Matrix . 1/determinant)
+        matrixMultWithConstant(adjointMatrix, 1.0 / determinant);
         
-            System.out.println("=".repeat((adjointMatrix.length * 8)+3) + "\n");
-        }
+        // create a reference just for clean coding, its not copy
+        double[][] inverseMatrix = adjointMatrix;
+        return inverseMatrix;
     }
-
-
 
     // Calculates the determinant of the square rxr matrix recursively
     // O(r) space complexity and O(n) time complexity where r=row number and n=number of elements in matrix(r^2)
@@ -457,7 +430,6 @@ public class Project1_Group17
         }
         return ans;
     }
-
     public static double[][] cofactorMatrix(double mat[][])
     {
         int row = mat.length;
@@ -482,7 +454,6 @@ public class Project1_Group17
     
         return cofactorMat;
     }
-
     public static double[][] ignore_ithRow_jthColumn(double mat[][], int i, int j)
     {
         int row = mat.length;
@@ -507,7 +478,6 @@ public class Project1_Group17
 
         return newMat;
     }
-
     public static void matrixMultWithConstant(double mat[][], double x)
     {
         int row = mat.length;
@@ -521,5 +491,150 @@ public class Project1_Group17
                 mat[i][j] *= x;
             }
         }
+    }
+
+    public static void Obj2Case3()
+    {
+        int row1 = takeInputOfRowAndColumn("number of ROWS for the first matrix");
+        int col1 = takeInputOfRowAndColumn("number of COLUMNS for the first matrix");
+        double mat1[][] = new double[row1][col1];
+        fillMatrix(mat1, row1, col1);
+
+        cls();
+        System.out.printf("Number of ROWS for the second matrix is set as %d to perform the task!!\n\n", col1);
+        try
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                System.out.println(".");
+                Thread.sleep(750);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("Error on pause!!");
+        }
+        cls();
+
+        int row2 = col1;
+        int col2 = takeInputOfRowAndColumn("number of COLUMNS for the second matrix");
+        double mat2[][] = new double[row2][col2];
+        fillMatrix(mat2, row2, col2);
+        cls();
+
+
+        printMatrix(mat1, row1, col1, "Your FIRST MATRIX:");
+        printMatrix(mat2, row2, col2, "Your SECOND MATRIX");
+
+        double multMat[][] = matrixMult(mat1, mat2);
+        printMatrix(multMat, multMat.length, multMat[0].length, "Multiplication of two matrices above:");
+        if (loopAsk())
+        {
+            loadingPage();
+            Obj2Case3();
+        }
+        else
+        {
+            loadingPage();
+            Objective2();
+        }
+    }
+    public static double[][] matrixMult(double mat1[][], double mat2[][])
+    {
+        int row1 = mat1.length;
+        int col1 = mat1[0].length;
+
+        int row2 = mat2.length;
+        int col2 = mat2[0].length;
+
+        int row_new = row1;
+        int col_new = col2;
+        double result[][] = new double[row_new][col_new];
+
+        // i is the row index for the new matrix
+        // j is the column index for the new matrix
+        // k is the index/counter for how many multiplication that must be summed to fill one spot in the new matrix
+        for(int i = 0; i < row_new; i++)
+        {
+            for(int j = 0; j < col_new; j++)
+            {
+                for(int k = 0; k < row2; k++)
+                {
+                    result[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void Obj2Case4()
+    {
+        int row = takeInputOfRowAndColumn("number of ROWS for both matrices");
+        int col = takeInputOfRowAndColumn("number of COLUMNS for both matrices");
+        double mat1[][] = new double[row][col];
+        double mat2[][] = new double[row][col];
+
+        cls();
+        System.out.println("You will fill the FIRST MATRIX!");
+        try
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                System.out.println(".");
+                Thread.sleep(750);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("Error on pause!!");
+        }
+        fillMatrix(mat1, row, col);
+
+        cls();
+        System.out.println("You will fill the SECOND MATRIX!");
+        try
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                System.out.println(".");
+                Thread.sleep(750);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("Error on pause!!");
+        }
+        fillMatrix(mat2, row, col);
+        cls();
+
+        double mult[][] = elementwiseMult(mat1, mat2);
+        printMatrix(mat1, row, col, "Your FIRST MATRIX:");
+        printMatrix(mat2, row, col, "Your SECOND MATRIX:");
+        printMatrix(mult, row, col, "Element-wise multiplication of the two matrices above:");
+
+        if (loopAsk())
+        {
+            loadingPage();
+            Obj2Case4();
+        }
+        else
+        {
+            loadingPage();
+            Objective2();
+        }
+    }
+    public static double[][] elementwiseMult(double mat1[][], double mat2[][])
+    {
+        int row = mat1.length;
+        int col = mat1[0].length;
+        double mult[][] = new double[row][col];
+
+        for(int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+                mult[i][j] = mat1[i][j] * mat2[i][j];
+        }
+
+        return mult;
     }
 }
